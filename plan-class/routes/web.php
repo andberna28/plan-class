2d6f1a87afa62ac167f6ec73d812ea32e3495e2d
Anderson Bernardo <bernardoanderson104@gmail.com>
2024-06-16 18:13:23 -0300

    Criação de views e alteração nas rotas

--- plan-class/routes/web.php
+++ plan-class/routes/web.php
@@ -1,16 +1,23 @@
 <?php
 
+use auth;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserRegisterController;
-use Illuminate\Support\Facades\Route;
 
 Route::get('/', [LoginController::class, 'index']);
 Route::post('/', [LoginController::class, 'login'])
     ->name('login');
 
-Route::get('logout', [LoginController::class, 'logout'])
-    ->name('logout')
-    ->middleware('auth');
+Route::middleware('auth')->group(function () {
+    Route::get('logout', [LoginController::class, 'logout'])
+        ->name('logout');
+    Route::get('/dashboard', [LoginController::class, 'dashboard'])
+        ->name('dashboard');
+    Route::get('painel', function() {
+        return view('dashboard');
+    })->name('dashboard');
+});
 
 Route::get('cadastro', function () {
     return view('register');
@@ -18,11 +25,3 @@ Route::get('cadastro', function () {
 
 Route::post('cadastro', [UserRegisterController::class, 'store'])
     ->name('register');
-
-Route::get('/dashboard', [LoginController::class, 'dashboard'])
-    ->middleware('auth');
-
-Route::get('painel', function() {
-    return view('dashboard');
-})->middleware('auth')
-  ->name('dashboard');
